2cf890115d667cec45080ac6fb1874215cd12ae7
cuisanzhang <cuisanzhang@163.com>
2017-07-23 03:29:35 +0800

    4.0发布版,详细的找过bug

--- app/src/main/java/com/cuisanzhang/mincreafting/ActivityTip.java
+++ app/src/main/java/com/cuisanzhang/mincreafting/ActivityTip.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ActivityTip extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class ActivityTip extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip);
-
+        initActionBar();
 //        Button btn_save_image = (Button) findViewById(R.id.btn_save_image);
 //        btn_save_image.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -19,5 +20,18 @@ public class ActivityTip extends AppCompatActivity {
 //
 //            }
 //        });
+    }
+
+    public void initActionBar() {
+        ImageView imageViewMenu = (ImageView)findViewById(R.id.imageViewToolbar_menu);
+        imageViewMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.webView_toolbar);
+//        toolbar.setTitle("");
+//        setSupportActionBar(toolbar);
     }
 }
