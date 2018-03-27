e8c801f59b80007cfb95f20af1dbcb02be6b4024
cuisanzhang <cuisanzhang@163.com>
2018-03-27 20:39:03 +0800

    Revert "Revert "更改清除缓存提示""
    This reverts commit 8a30690db507cefb35572348a6c6f1dd8e10e1bf.

--- app/src/main/java/com/cuisanzhang/mincreafting/MyDiskLruCache.java
+++ app/src/main/java/com/cuisanzhang/mincreafting/MyDiskLruCache.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.jakewharton.disklrucache.DiskLruCache;
 
@@ -58,7 +59,7 @@ public class MyDiskLruCache {
 
 
     //优先外部缓存
-    private   File getDiskCacheDir(Context context, String uniqueName) {
+    public    File getDiskCacheDir(Context context, String uniqueName) {
         String cachePath;
 //        Log.e("cachePath", cachePath);
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
@@ -74,6 +75,7 @@ public class MyDiskLruCache {
         if (!cacheDir.exists()) {
             cacheDir.mkdirs();
         }
+        Toast.makeText(context, cacheDir.getAbsolutePath(), Toast.LENGTH_LONG).show();
         Log.e("getCacheDir", cacheDir.getAbsolutePath());
         return cacheDir;
     }
